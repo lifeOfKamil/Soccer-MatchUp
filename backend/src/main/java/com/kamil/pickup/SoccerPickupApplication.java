8aee9a85d753b0ebe0c331f87b410983e3930241
lifeOfKamil <lepkowskikamil@yahoo.com>
2025-07-12 19:51:19 -0500

    Day 0: Backend setup with Maven and Spring

---
+++ backend/src/main/java/com/kamil/pickup/SoccerPickupApplication.java
@@ -0,0 +1,13 @@
+package com.kamil.pickup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerPickupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccerPickupApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/kamil/pickup/SoccerPickupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamil.pickup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccerPickupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
