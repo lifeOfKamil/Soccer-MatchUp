c8da218e2ecdef1373f901ff7bd5173c13fb0db8
lifeOfKamil <lepkowskikamil@yahoo.com>
2025-07-22 18:30:43 -0500

    Fix: Update SoccerPickupApplicationTests profile

--- backend/src/test/java/com/kamil/pickup/SoccerPickupApplicationTests.java
+++ backend/src/test/java/com/kamil/pickup/SoccerPickupApplicationTests.java
@@ -2,8 +2,10 @@ package com.kamil.pickup;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class SoccerPickupApplicationTests {
 
 	@Test
